51dc84b0b3df055f1a20073735d076421527623a
kinjal <kinjal@itoneclick.com>
2017-05-05 19:42:38 +0530

    update logout method184

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,22 +6,21 @@ class Restaurant < ActiveRecord::Base
 
 	 geocoded_by :formatted_address, :skip_index => true
 	 after_validation :geocode 
-	 after_create :add_latitude_longitude 
-	 # after_update :add_latitude_longitude 
+	 # after_save :add_latitude_longitude 
 
 
-	 def add_latitude_longitude
-	 	if !self.formatted_address.blank?
-	 		coordinates = Geocoder.coordinates(self.formatted_address)
-	 		if !coordinates.blank?
-		 		# self.latitude = coordinates[0]
-		 		# self.longitude = coordinates[1]
-		 		# self.save
-		 		self.update_attributes(:latitude => coordinates[0], :longitude => coordinates[1])
+	 # def add_latitude_longitude
+	 # 		coordinates = Geocoder.coordinates(self.formatted_address)
+	 # 		if !coordinates.blank?
+		#  		# self.latitude = coordinates[0]
+		#  		# self.longitude = coordinates[1]
+		#  		# self.save
+		#  		puts self.changed[1]
+		#  		# self.update_attributes(:latitude => coordinates[0], :longitude => coordinates[1])
 
-		 	end
-	 	end
-	 end
+		#  	end
+	 	
+	 # end
 	rails_admin do
  		list do
  			field :name
