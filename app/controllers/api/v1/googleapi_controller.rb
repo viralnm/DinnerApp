2dd74c52b03de83eee32bca7a0df9de2991bc0d0
kinjal <kinjal@itoneclick.com>
2017-05-05 12:51:47 +0530

    add sorted data api

--- app/controllers/api/v1/googleapi_controller.rb
+++ app/controllers/api/v1/googleapi_controller.rb
@@ -15,15 +15,47 @@ class Api::V1::GoogleapiController < Api::BaseController
 	end
 	def newplace
 		@key = "AIzaSyCQ9aGFwsgl4IsJqpY5HHdnDbTWBHyD_TQ"
-	#  type = "cafe"
-	#  radius = 500
-
-	#  @response=RestClient::Request.execute(method: :get, url: 'https://maps.googleapis.com/maps/api/place/textsearch/json',
- #                            timeout: 10, headers: {params: {key: @key, location: params[:location],:radius => radius, :type => type}})
-	# @response = ActiveSupport::JSON.decode(@response)
+	 type = "cafe"
+	 radius = 500
+	 @array = Array.new
+	 @response_google=RestClient::Request.execute(method: :get, url: 'https://maps.googleapis.com/maps/api/place/textsearch/json',
+                            timeout: 10, headers: {params: {key: @key, location: params[:location],:radius => radius, :type => type}})
+	@response_google = ActiveSupport::JSON.decode(@response_google)
 
     @response = Restaurant.near([params[:latitude], params[:longitude]], 500)
-  Geocoder::Calculations.distance_between([47.858205,2.294359], [40.748433,-73.985655])
+  
+  #push google result in array 
+  	if @response_google['results'].size > 0
+  		@response_google['results'].each do |res|
+  			photo = Array.new
+  			if !res['photos'].blank?
+  				res['photos'].each do |ph|
+  					photo << {photo_url: "https://maps.googleapis.com/maps/api/place/photo?maxwidth="+ph['width'].to_s+"&photoreference="+ph['photo_reference']+"&key="+@key.to_s, photoreference: ph['photo_reference']}
+  				end
+  			else
+  				photo << res['photos']
+  			end
+  			distance = Geocoder::Calculations.distance_between([params[:latitude], params[:longitude]], [res['geometry']['location']['lat'],res['geometry']['location']['lng']])
+  			@array << {name: res['name'], formatted_address: res['formatted_address'], latitude: res['geometry']['location']['lat'], longitude: res['geometry']['location']['lng'], place_id: res['place_id'], rating: res['rating'], distance: distance, photo: photo , add_manual: false}
+  		end
+  	end
+  # push local response in array
+  	if  @response.size > 0
+  		@response.each do |res|
+  			photo = Array.new
+  			db_photos = res.restaurant_photos.all
+  			if !db_photos.blank?
+  				db_photos.each do |ph|
+  					photo << {photo_url: ph.photo.url, photoreference: ph.id}
+  				end
+  			else
+  				photo << db_photos
+  			end
+  			@array << {name: res.name, formatted_address: res.formatted_address, latitude: res.latitude, longitude: res.longitude, place_id: res.id, rating: res['rating'], distance: res.distance, photo: photo , add_manual: true}
+  		end
+  	end
 
+
+  	@sorted = @array.sort_by { |k| k[:distance] }
 	end
 end
--- app/views/api/v1/googleapi/newplace.json.jbuilder
+++ app/views/api/v1/googleapi/newplace.json.jbuilder
@@ -1,9 +1,10 @@
 json.action "googleapi_place"
-if @response.size > 0
+if @sorted.size > 0
 	json.response "true"
 	json.msg  "Successfully fetch your googleapi_place details"
+	json.results_google @sorted
 else
 	json.response "false"
 	json.msg  "places are not available."
 end
-json.results @response
+
