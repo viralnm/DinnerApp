917b3ce94364446f044d72ac7ee4e1b74d454c97
kinjal <kinjal@itoneclick.com>
2017-05-10 16:58:17 +0530

    update photo logic115

--- app/models/restaurant_photo.rb
+++ app/models/restaurant_photo.rb
@@ -1,6 +1,8 @@
 class RestaurantPhoto < ActiveRecord::Base
 	belongs_to :restaurant
-	has_attached_file :photo, :styles => {:avatar => "100x100#"}
+	has_attached_file :photo, :styles => {:avatar => "100x100#"},
+	:path => "/public/rest/img/:id/:style/:basename.jpg",
+	:url => "/public/rest/img/:id/:style/:basename.jpg"
 	rails_admin do
  		list do
  			field :restaurant
