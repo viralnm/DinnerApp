e2de60749c2d16d21789920333bac06267ce268f
kinjal <kinjal@itoneclick.com>
2017-05-10 16:38:34 +0530

    update photo logic122

--- app/controllers/api/v1/googleapi_controller.rb
+++ app/controllers/api/v1/googleapi_controller.rb
@@ -119,7 +119,7 @@ class Api::V1::GoogleapiController < Api::BaseController
   					# url_c = ph.photo.url
       	# 		@s_urls = RestClient.post "https://www.googleapis.com/urlshortener/v1/url/?key=AIzaSyBU_beRXw4pisGLzJLlwMAdKaBhj3XTYCY", {"longUrl" => url_c}.to_json, :content_type => "application/json"
   					# @s_urls = ActiveSupport::JSON.decode(@s_urls)
-  					image_url = ph.photo.url
+  					image_url = "http://54.218.251.105:3000"+ph.photo.url
 						api_key = 'acc_61d09fb31788cb1'
 						api_secret = 'e818bc86ebe0f859b8d3a56233578ce0'
 						auth = 'Basic ' + Base64.strict_encode64( "#{api_key}:#{api_secret}" ).chomp
